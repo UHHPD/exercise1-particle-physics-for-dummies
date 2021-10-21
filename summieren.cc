e65e7babd5a157e22d5c48ea10a5d42e12038c05
luzio33 <>
2021-10-21 10:56:39 +0000

    Summe in file

--- summieren.cc
+++ summieren.cc
@@ -6,21 +6,24 @@ int main()
   int i;
   int sum;
   std::ifstream fin("daten.txt");
-  //for (i = 0; i < 2; i++){
+  std::ofstream fout("datensumme.txt");
   while(! fin.eof()){
-  for (i = 0; i < 2; i++) {
-    fin >> zahl ;
-    if (i==0){
-    sum = zahl;
-    std::cout << zahl << " + ";
+    for (i = 0; i < 2; i++) {
+      fin >> zahl ;
+      if (i==0){
+      sum = zahl;
+      //fout << zahl << " + ";
+      }
+      else{
+        sum += zahl;
+      //fout << zahl << " ";
     }
-    else{
-      sum += zahl;
-    std::cout << zahl << " ";
-    }
-  //  std::cout <<  std::endl;
-    }
-    std::cout << "= " << sum <<std::endl;
+  }
+    fout << sum <<std::endl;
   }
   fin.close();
+  fout.close();
 }
+
+
+  
