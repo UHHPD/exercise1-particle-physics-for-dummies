51f35ec95404ee64974aa04b832de98ab05b9588
luzio33 <54103941+luzio33@users.noreply.github.com>
2021-10-21 10:48:58 +0000

    Exercise 2.4 works

--- summieren.cc
+++ summieren.cc
@@ -4,11 +4,23 @@ int main()
 {
   int zahl;
   int i;
+  int sum;
   std::ifstream fin("daten.txt");
-  for (i = 0; i < 2; i++){
-  //while(! fin.eof()){
+  //for (i = 0; i < 2; i++){
+  while(! fin.eof()){
+  for (i = 0; i < 2; i++) {
     fin >> zahl ;
-  std::cout << zahl << std::endl;
+    if (i==0){
+    sum = zahl;
+    std::cout << zahl << " + ";
+    }
+    else{
+      sum += zahl;
+    std::cout << zahl << " ";
+    }
+  //  std::cout <<  std::endl;
+    }
+    std::cout << "= " << sum <<std::endl;
   }
   fin.close();
 }
