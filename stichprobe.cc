65db3173d27af8de6e2db808fd46ef0c25518974
luzio33 <54103941+luzio33@users.noreply.github.com>
2021-10-28 11:14:37 +0000

    2.1 done

--- stichprobe.cc
+++ stichprobe.cc
@@ -1,5 +1,6 @@
 #include <fstream>
 #include <iostream>
+#include <math.h>
 int main()
 {
   std::ifstream fin("datensumme.txt");
@@ -12,4 +13,12 @@ int main()
   }
   mean=mean/N;
   std :: cout << mean << std :: endl ;
+  float var = 0;
+  for(int i=0; i<N; i++){
+    var+=pow(a[i]-mean,2);
+  }
+  var=var/N;
+  std :: cout << var << std :: endl ;
+  float stddev = sqrt(var);
+  std :: cout << stddev << std :: endl ;
 }
