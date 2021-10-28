d1ed75139e578f636cbcf47ef30e427e8ea77bea
luzio33 <54103941+luzio33@users.noreply.github.com>
2021-10-28 11:34:34 +0000

    2.2a 1st trz

---
+++ stichprobe2.cc
@@ -0,0 +1,39 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
+int main()
+{
+  std::ifstream fin("datensumme.txt");
+  std::ofstream fout("mittelwerte.txt");
+  std::ofstream fout2("varianzen.txt");
+  int N = 234;
+  int M =9;
+  int a[N][M];
+  int i=0;
+  while(!fin.eof()){
+    float mean = 0;
+    for(int j=0; j<M; j++){
+      fin >> a[i][j];
+      mean += a[i][j];
+    }
+    if(!fin.eof()){
+      mean=mean/M;
+      fout << "Mittelwert: " << mean << std :: endl ;
+      float var = 0;
+      
+      for(int j=0; j<M; j++){
+        var+=pow(a[i][j]-mean,2);
+      }
+      var=var/M;
+      fout2 << "Varianz: " <<var << std :: endl ;
+
+      float stddev = sqrt(var);
+      //std :: cout << "Standardabweichung: "<<stddev << std :: endl ;
+      
+      i++;
+    }
+  }
+  fin.close();
+  fout.close();
+  fout2.close();
+}
