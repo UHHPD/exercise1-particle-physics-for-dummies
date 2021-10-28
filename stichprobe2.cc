7eded890ec775b25c18fed7e579869045f6454fd
luzio33 <54103941+luzio33@users.noreply.github.com>
2021-10-28 11:50:43 +0000

    2 done. What is wrong with 2.2a?

--- stichprobe2.cc
+++ stichprobe2.cc
@@ -8,31 +8,39 @@ int main()
   std::ofstream fout2("varianzen.txt");
   int N = 234;
   int M =9;
+  int O = N/M;
   int a[N][M];
   int i=0;
+  float mean[O];
+  float Mittelwert;
+  float var[O];
+  float Varianz;
   while(!fin.eof()){
-    float mean = 0;
     for(int j=0; j<M; j++){
       fin >> a[i][j];
-      mean += a[i][j];
+      mean[i] += a[i][j];
     }
     if(!fin.eof()){
-      mean=mean/M;
-      fout << mean << std :: endl ;
-      float var = 0;
+      mean[i]=mean[i]/M;
+      fout << mean[i] << std :: endl ;
       
       for(int j=0; j<M; j++){
-        var+=pow(a[i][j]-mean,2);
+        var[i]+=pow(a[i][j]-mean[i],2);
       }
-      var=var/M;
-      fout2 <<var << std :: endl ;
+      var[i]=var[i]/M;
+      fout2 << var[i] << std :: endl ;
 
-      float stddev = sqrt(var);
+      float stddev = sqrt(var[i]);
       //std :: cout << "Standardabweichung: "<<stddev << std :: endl ;
-      
+      Mittelwert += mean[i];
+      Varianz += var[i];
       i++;
     }
   }
+  std :: cout << "Mittelwert der 9er Blöcke: "<< Mittelwert/O << std :: endl ;
+  std :: cout << "Varianz der 9er Blöcke: "<< Varianz/O << std :: endl ;
+  std :: cout << "Varianz der 9er Blöcke mit Bessel's Correction: "<< Varianz/(O-1) << std :: endl ;
+
   fin.close();
   fout.close();
   fout2.close();
